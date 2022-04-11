95d9734d31755b911fccc91a320ab123da7ae19d
TheSharpOwl <13377190+TheSharpOwl@users.noreply.github.com>
2022-04-11 03:23:57 +0300

    Change the first message.

--- config.py
+++ config.py
@@ -13,6 +13,7 @@ class DefaultConfig:
     PORT = 3978
     APP_ID = os.environ.get("MicrosoftAppId", "")
     APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "")
+    # You can put the knowledge base qna service keys here (put them as environment variables in your os and they will be subsitituded here)
     QNA_KNOWLEDGEBASE_ID = os.environ.get("QnAKnowledgebaseId", "")
     QNA_ENDPOINT_KEY = os.environ.get("QnAEndpointKey", "")
     QNA_ENDPOINT_HOST = os.environ.get("QnAEndpointHostName", "")
